71714eba41fa15c82403fc41b503b69ad82b78ce
AlexP <alex.pfanne1@gmail.com>
2019-11-12 12:37:05 +0100

    11723 - Numbering Roads

--- 11723 - Numbering Roads/NumberingRoads.java
+++ 11723 - Numbering Roads/NumberingRoads.java
@@ -12,9 +12,8 @@ public class NumberingRoads {
 
             if (r == 0 && n == 0) break;
 
-            int k;
-            if (r % n == 0) k = r/n - 1;
-            else k = r/n;
+            int k = (int)Math.ceil((r-n)/(double)n); // TODO:
+
 
             if (k <= 26) System.out.println("Case " + i + ": " + k);
             else System.out.println("Case " + i + ": impossible");
