5a6809465715a047aba09e224d71ee2ce073d788
AlexP <alex.pfanne1@gmail.com>
2019-11-15 07:25:11 +0100

    272 - TEX Quotes

---
+++ 272 - TEX Quotes/TexQuotes.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        boolean replace = true;
+        while(s.hasNextLine()) {
+
+            String line = s.nextLine();
+            for(int i = 0; i < line.length(); i++) {
+                if(line.charAt(i) == '\"') {
+                    if(replace) System.out.print("``");
+                    else System.out.print("''");
+                    replace = !replace;
+                } else
+                    System.out.print(line.charAt(i));
+            }
+
+            System.out.println();
+        }
+
+        s.close();
+
+    }
+}
