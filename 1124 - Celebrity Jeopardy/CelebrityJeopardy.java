5e7ce161f35929bfce194e25143599fc5c7d49de
AlexP <alex.pfanne1@gmail.com>
2019-11-15 07:31:10 +0100

    1124 - Celebrity Jeopardy

---
+++ 1124 - Celebrity Jeopardy/CelebrityJeopardy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CelebrityJeopardy {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(s.hasNext()) {
+            System.out.println(s.nextLine());
+        }
+        s.close();
+    }
+}
--- 272 - TEX Quotes/TexQuotes.java
+++ 272 - TEX Quotes/TexQuotes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class TexQuotes {
 
     public static void main(String[] args) {
 
