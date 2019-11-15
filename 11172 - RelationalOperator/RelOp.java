495e9569d6d40807ea69475807417eac37aa6a32
AlexP <alex.pfanne1@gmail.com>
2019-11-15 08:42:23 +0100

    11172 - RelationalOperator

---
+++ 11172 - RelationalOperator/RelOp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            if(a > b) System.out.println(">");
+            else if(a < b) System.out.println("<");
+            else System.out.println("=");
+        }
+
+        s.close();
+    }
+}
