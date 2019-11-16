41b0960b08ef8806620039626a1f5295b928ba9c
AlexP <alex.pfanne1@gmail.com>
2019-11-16 09:11:31 +0100

    893 - Y3K

---
+++ 893 - Y3K/Y3K.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        while(true) {
+            int toPredict = s.nextInt();
+            int days = s.nextInt();
+            int month = s.nextInt();
+            int year = s.nextInt();
+            if(toPredict == 0 && days == 0 && month == 0 && year == 0) break;
+
+            LocalDate d = LocalDate.of(year, month, days);
+            d = d.plusDays(toPredict);
+            System.out.println(d.getDayOfMonth() + " " + d.getMonthValue() + " " + d.getYear());
+        }
+
+        s.close();
+    }
+}
