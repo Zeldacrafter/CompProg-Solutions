9ac2474b8966a8a0426b45824bb58f0ee00d84a9
AlexP <alex.pfanne1@gmail.com>
2019-11-26 13:29:25 +0100

    12478 - Hardest Problem Ever (Easy)

---
+++ 12478 - Hardest Problem Ever (Easy)/12478.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+
+#define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
+#define FORI(a, b) for (auto(a) = (b).begin(); (a) != (b).end(); ++(a))
+
+int main() {
+  printf("KABIR\n");
+  return 0;
+}
