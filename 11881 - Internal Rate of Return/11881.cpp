88e0cf89baf428d6afb75338aa5db949f6dac0bf
AlexP <alex.pfanne1@gmail.com>
2020-01-14 13:14:00 +0100

    11881 - Internal Rate of Return + Template file

--- 11881 - Internal Rate of Return/11881.cpp
+++ 11881 - Internal Rate of Return/11881.cpp
@@ -32,9 +32,50 @@ const int xd[4] = {0, 1, 0, -1}, yd[4] = {1, 0, -1, 0};
 template <typename t> bool ckmin(t& a, const t& b) { return a > b ? a = b, true : false; }
 template <typename t> bool ckmax(t& a, const t& b) { return a < b ? a = b, true : false; }
 
+const double EPS = 1e-9;
+
+double calc(vi& cfs, double x) {
+    double res = 0;
+    double den = 1;
+    F0R(i, SZ(cfs)) {
+        res += cfs[i]/den;
+        den *= (1 + x);
+    }
+    return res;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+
+    while(true) {
+        int t;
+        cin >> t;
     
+        if(!t) break;
+        
+        vi cfs(t + 1, -1);
+        FOR(i, 0, t + 1)
+            cin >> cfs[i];
+
+        double low = -1.0;
+        double high = 1.0;
+        while(calc(cfs, high) > 0)
+            high *= 2;
+
+        while(low < high) {
+            double mid = MID(low, high);
+            double res = calc(cfs, mid);
+            if(abs(res) < EPS)
+                break;
+            else if(res > 0)
+                low = mid;
+            else
+                high = mid;
+        }
+
+        cout << fixed << setprecision(2) << MID(low, high) << '\n';
+    }
+
     return 0;
 }
