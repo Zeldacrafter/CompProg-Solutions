0ed2ca4783313c72b16a9f8210289c094c54c0cd
AlexP <alex.pfanne1@gmail.com>
2020-04-01 21:42:04 +0200

    LeetCode: 30 Day Challange, Day 1

---
+++ LeetCode/30 Day Challange/1-single_number.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(int n : nums)
+            res ^= n;
+        return res;
+    }
+};
