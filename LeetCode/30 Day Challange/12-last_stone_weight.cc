98ca36c99e451bbfe612bc77ce5c9bfaf0b580f2
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-04-12 13:41:01 +0200

    LeetCode: 30 Day Challange, Day 12

---
+++ LeetCode/30 Day Challange/12-last_stone_weight.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int, vector<int>> q;
+        for(int s : stones)
+            q.push(s);
+        while(q.size() > 1) {
+            int a = q.top(); q.pop();
+            int b = q.top(); q.pop();
+            if(a - b)
+                q.push(abs(a - b));
+        }
+        return q.empty() ? 0 : q.top();
+    }
+};
