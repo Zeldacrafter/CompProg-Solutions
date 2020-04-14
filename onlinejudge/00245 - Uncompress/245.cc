0635ec03c7c40eb8fa1a47e3b00e3c60ebe2865d
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-04-14 19:22:57 +0200

    onlinejudge: 245 - Uncompress

---
+++ onlinejudge/00245 - Uncompress/245.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define pf push_front
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ge(a, b) get<(b)>((a))
+#define SZ(x) (x).size()
+#define endl '\n'
+#define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
+#define ROF(a, b, c) for (auto(a) = (b); (a) > (c); --(a))
+#define F0R(a, b) FOR ((a), 0, (b))
+#define R0F(a, b) ROF ((a), (b), -1)
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+#define CLR(a, b) memset((a), (b), sizeof((a)))
+template <typename t> bool ckmin(t& a, const t& b) {return a > b ? a = b, true : false;}
+template <typename t> bool ckmax(t& a, const t& b) {return a < b ? a = b, true : false;}
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dout \
+if (DEBUG) std::cerr
+
+int main() {
+
+    string inp;
+    while(1) {
+        char c = getchar();
+        if(c == '0' && (!SZ(inp) || !(inp[SZ(inp) - 1] >= '0' && inp[SZ(inp) - 1] <= '9')))
+            break;
+        inp += c;
+    }
+    int n = SZ(inp);
+
+    vector<string> text;
+    string res, s;
+    for(int i = 0; i < n; ) {
+        while(i < n && ((inp[i] >= 'a' && inp[i] <= 'z') || (inp[i] >= 'A' && inp[i] <= 'Z')))
+            s.pb(inp[i++]);
+        if(SZ(s)) {
+            text.pb(s);
+            res += s;
+            s.clear();
+        }
+
+        while(i < n && (inp[i] >= '0' && inp[i] <= '9'))
+            s.pb(inp[i++]);
+        if(SZ(s)) {
+            int offset = stoi(s);
+            string next = text[SZ(text) - offset];
+
+            text.erase(text.end() - offset);
+            text.pb(next);
+
+            res += next;
+            s.clear();
+        }
+        
+        while(i < n && !(inp[i] >= '0' && inp[i] <= '9') 
+                    && !(inp[i] >= 'a' && inp[i] <= 'z') 
+                    && !(inp[i] >= 'A' && inp[i] <= 'Z'))
+            res += inp[i++];
+    }
+    cout << res;
+}
+
