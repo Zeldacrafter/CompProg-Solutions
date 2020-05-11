9e37b4f0d6df6b3c39cbb7dfe6e721e42c8c4b9a
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-05-11 17:38:49 +0200

    onlinejudge: 10934 - Dropping Water Balloons

---
+++ onlinejudge/10934 - Dropping Water Balloons/10934.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define SZ(x) (int)(x).size()
+#define endl '\n'
+#define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
+#define F0R(a, b) FOR ((a), 0, (b))
+#define ROF(a, b, c) for (auto(a) = (b); (a) > (c); --(a))
+#define R0F(a, b) ROF ((a), (b), -1)
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+#define SET(a, b) memset((a), (b), sizeof(a))
+template <class t> bool ckmin(t& a, const t& b) { return a > b ? a = b, true : false; }
+template <class t> bool ckmax(t& a, const t& b) { return a < b ? a = b, true : false; }
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dout \
+  if (DEBUG) std::cerr
+#define dvar(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+template <class a, class b>
+ostream& operator<<(ostream& o, const pair<a, b>& p);
+template <class T> struct IsC {
+  template <class C> static char test(typename C::const_iterator*);
+  template <class C> static int test(...);
+  static const bool value = sizeof(test<T>(0)) == sizeof(char);
+};
+template <> struct IsC<string> { static const bool value = false; };
+template <class C> typename enable_if<IsC<C>::value, ostream&>::type operator<<( ostream& o, const C& c) {
+  o << '{';
+  for (auto it = c.begin(); it != c.end();)
+    o << *it << (++it != c.end() ? ", " : "}");
+  return o;
+}
+template <class a, class b> ostream& operator<<(ostream& o, const pair<a, b>& p) {
+  return o << '(' << p.fi << ", " << p.se << ')';
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    vector<vector<ll>> dp(101, vector<ll>(64, 0));
+    FOR(i, 1, 101) {
+        FOR(j, 1, 64) {
+            if(j == 1)
+                dp[i][j] = 1;
+            else
+                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1] + 1;
+        }
+    }
+    ll n, k;
+    while(1) {
+        cin >> k >> n;
+        if(!k)
+            break;
+
+        auto it = lower_bound(ALL(dp[k]), n);
+        if(it == dp[k].end())
+            cout << "More than 63 trials needed.\n";
+        else
+            cout << it - dp[k].begin() << endl;
+
+
+    }
+}
