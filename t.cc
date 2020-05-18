86384aa18c4c03dcee8a8f9c8a28614f4f67a017
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-05-18 14:57:57 +0200

    t.cc

--- t.cc
+++ t.cc
@@ -1,6 +1,6 @@
-// this line is here for a reason
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<int> vi;
@@ -11,29 +11,20 @@ typedef vector<vii> vvii;
 #define se second
 #define ALL(x) (x).begin(), (x).end()
 #define RALL(x) (x).rbegin(), (x).rend()
-#define pf push_front
-#define eb emplace_back
 #define pb push_back
+#define eb emplace_back
 #define mp make_pair
-#define mt make_tuple
 #define SZ(x) (int)(x).size()
-#define ft front()
-#define bk back()
 #define endl '\n'
 #define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
-#define ROF(a, b, c) for (auto(a) = (b); (a) > (c); --(a))
 #define F0R(a, b) FOR ((a), 0, (b))
+#define ROF(a, b, c) for (auto(a) = (b); (a) > (c); --(a))
 #define R0F(a, b) ROF ((a), (b), -1)
 #define CEIL(a, b) ((a) + (b)-1) / (b)
 #define SET(a, b) memset((a), (b), sizeof(a))
-template <class t>
-bool ckmin(t& a, const t& b) {
-  return a > b ? a = b, true : false;
-}
-template <class t>
-bool ckmax(t& a, const t& b) {
-  return a < b ? a = b, true : false;
-}
+template <class t> bool ckmin(t& a, const t& b) { return a > b ? a = b, true : false; }
+template <class t> bool ckmax(t& a, const t& b) { return a < b ? a = b, true : false; }
+
 #ifndef DEBUG
 #define DEBUG 0
 #endif
@@ -42,28 +33,19 @@ bool ckmax(t& a, const t& b) {
 #define dvar(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 template <class a, class b>
 ostream& operator<<(ostream& o, const pair<a, b>& p);
-template <class T>
-struct IsC {
-  template <class C>
-  static char test(typename C::const_iterator*);
-  template <class C>
-  static int test(...);
+template <class T> struct IsC {
+  template <class C> static char test(typename C::const_iterator*);
+  template <class C> static int test(...);
   static const bool value = sizeof(test<T>(0)) == sizeof(char);
 };
-template <>
-struct IsC<string> {
-  static const bool value = false;
-};
-template <class C>
-typename enable_if<IsC<C>::value, ostream&>::type operator<<(
-    ostream& o, const C& c) {
+template <> struct IsC<string> { static const bool value = false; };
+template <class C> typename enable_if<IsC<C>::value, ostream&>::type operator<<( ostream& o, const C& c) {
   o << '{';
   for (auto it = c.begin(); it != c.end();)
     o << *it << (++it != c.end() ? ", " : "}");
   return o;
 }
-template <class a, class b>
-ostream& operator<<(ostream& o, const pair<a, b>& p) {
+template <class a, class b> ostream& operator<<(ostream& o, const pair<a, b>& p) {
   return o << '(' << p.fi << ", " << p.se << ')';
 }
 
