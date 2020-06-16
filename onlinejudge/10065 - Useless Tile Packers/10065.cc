5f030c4bfffc9ce6338d5c53abf8a20cdf1e6f4b
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-06-16 19:20:53 +0200

    onlinejudge: 10065 - Useless Tile Packers

---
+++ onlinejudge/10065 - Useless Tile Packers/10065.cc
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define SZ(x) (int)(x).size()
+#define endl '\n'
+#define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
+#define F0R(a, b) FOR ((a), 0, (b))
+#define ROF(a, b, c) for (auto(a) = (b); (a) > (c); --(a))
+#define R0F(a, b) ROF ((a), (b), -1)
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+#define SET(a, b) memset((a), (b), sizeof(a))
+template <class t> bool ckmin(t& a, const t& b) { return a > b ? a = b, true : false; }
+template <class t> bool ckmax(t& a, const t& b) { return a < b ? a = b, true : false; }
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dout \
+  if (DEBUG) std::cerr
+#define dvar(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+template <class a, class b>
+ostream& operator<<(ostream& o, const pair<a, b>& p);
+template <class T> struct IsC {
+  template <class C> static char test(typename C::const_iterator*);
+  template <class C> static int test(...);
+  static const bool value = sizeof(test<T>(0)) == sizeof(char);
+};
+template <> struct IsC<string> { static const bool value = false; };
+template <class C> typename enable_if<IsC<C>::value, ostream&>::type operator<<( ostream& o, const C& c) {
+  o << '{';
+  for (auto it = c.begin(); it != c.end();)
+    o << *it << (++it != c.end() ? ", " : "}");
+  return o;
+}
+template <class a, class b> ostream& operator<<(ostream& o, const pair<a, b>& p) {
+  return o << '(' << p.fi << ", " << p.se << ')';
+}
+
+#define xx real()
+#define yy imag()
+using pt = complex<int>;
+int dot(pt a, pt b) { return a.xx * b.xx + a.yy * b.yy; }
+int cross(pt a, pt b) { return a.xx * b.yy - a.yy * b.xx; }
+int dir(pt a, pt b, pt c) { return cross(b - a, c - a); }
+bool cw(pt a, pt b, pt c) { return dir(a, b, c) < 0; }
+bool ccw(pt a, pt b, pt c) { return dir(a, b, c) > 0; }
+
+vector<pt> convexHull(vector<pt>& pts) {
+  int n = SZ(pts);
+  sort(ALL(pts),
+       [](pt a, pt b) { return mp(a.xx, a.yy) < mp(b.xx, b.yy); });
+  vector<pt> up, down;
+  up.eb(pts[0]); down.eb(pts[0]);
+  FOR (i, 1, n) {
+    if (i == n - 1 || ccw(pts[0], pts[n - 1], pts[i])) {
+      while (SZ(up) > 1 &&
+             ccw(up[SZ(up) - 2], up[SZ(up) - 1], pts[i]))
+        up.pop_back();
+      up.eb(pts[i]);
+    }
+    if (i == n - 1 || cw(pts[0], pts[n - 1], pts[i])) {
+      while (SZ(down) > 1 &&
+             cw(down[SZ(down) - 2], down[SZ(down) - 1], pts[i]))
+        down.pop_back();
+      down.eb(pts[i]);
+    }
+  }
+  vector<pt> ans(up);
+  ans.insert(ans.end(), 1 + RALL(down));
+  return ans;
+}
+
+double area(vector<pt>& pts) {
+    double res = 0;
+    F0R(i, SZ(pts))
+        res += cross(pts[i], pts[(i+1)%SZ(pts)]);
+    return abs(res) / 2;
+}
+
+void solve(int n) {
+
+    vector<pt> pts;
+    F0R(i, n) {
+        int x, y;
+        cin >> x >> y;
+        pts.eb(x, y);
+    }
+
+    double small = area(pts);
+
+    vector<pt> hull = convexHull(pts);
+    hull.pop_back();
+
+    double big = area(hull);
+
+    cout << "Wasted Space = " << setprecision(2) << fixed << 100 - small/big*100 << " %\n\n";
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    for(int tc = 1, n; cin >> n, n;++tc) {
+        cout << "Tile #" << tc << endl;
+        solve(n);
+    }
+}
