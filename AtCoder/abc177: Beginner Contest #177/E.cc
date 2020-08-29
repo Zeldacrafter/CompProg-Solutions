2f26c99d77beec95f0837f4e06634437cff34ce1
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-08-29 15:46:12 +0200

    AtCoder: Beginner Contest #177 (A, B, C, D, E)

---
+++ AtCoder/abc177: Beginner Contest #177/E.cc
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define SZ(x) (int)(x).size()
+#define endl '\n'
+#define FOR(a, b, c) for (auto a = (b); (a) < (c); ++(a))
+#define F0R(a, b) FOR (a, 0, (b))
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return a > b ? a = b, true : false;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, true : false;
+}
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dout \
+  if (DEBUG) std::cerr
+#define dvar(...) " [" << #__VA_ARGS__ ": " << mt(__VA_ARGS__) << "] "
+template <typename T>
+struct IsC {
+  template <typename C>
+  static char test(typename C::const_iterator*);
+  template <typename C>
+  static int test(...);
+  static const bool value = sizeof(test<T>(0)) == sizeof(char);
+};
+template <>
+struct IsC<string> {
+  static const bool value = false;
+};
+template <typename C>
+typename enable_if<IsC<C>::value, ostream&>::type operator<<(ostream&, const C&);
+template <typename T1, typename T2>
+ostream& operator<<(ostream&, const pair<T1, T2>&);
+template <typename... T> using TS = tuple_size<tuple<T...>>;
+template<size_t idx, typename... T>
+typename enable_if<TS<T...>::value == idx + 1, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return o << ", " << get<idx>(t) << ')';
+}
+template<size_t idx, typename... T>
+typename enable_if<0 < idx && idx + 1 < TS<T...>::value, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return operator<<<idx + 1>(o << ", " << get<idx>(t), t);
+}
+template<size_t idx = 0, typename... T>
+typename enable_if<1 < TS<T...>::value && !idx, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return operator<<<idx + 1>(o << '(' << get<idx>(t), t);
+}
+template<typename T>
+ostream& operator<<(ostream& o, const tuple<T>& t) {
+  return o << get<0>(t);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& o, const pair<T1, T2>& p) {
+  return o << mt(p.fi, p.se);
+}
+template <typename C>
+typename enable_if<IsC<C>::value, ostream&>::type operator<<(
+    ostream& o, const C& c) {
+  o << '[';
+  for (auto it = c.cbegin(); it != c.cend(); ++it)
+    o << *it << (next(it) != c.cend() ? ", " : "");
+  return o << ']';
+}
+template <typename T>
+void tprint(vector<vector<T>>& v, size_t width = 0, ostream& o = cerr) {
+  if (!DEBUG) return;
+  for (const auto& vv : v) {
+    for (const auto& i : vv) o << setw(width) << i;
+    o << endl;
+  }
+}
+
+const int N = 1e7;
+int lp[N + 1];
+void sieve() {
+  vi pr;
+  FOR (i, 2, N + 1) {
+    if (!lp[i]) {
+      lp[i] = i;
+      pr.pb(i);
+    }
+    for (int j = 0; j < SZ(pr) && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+      lp[i * pr[j]] = pr[j];
+  }
+}
+
+vi seen((int)1e7);
+void solve() {
+    int n;
+    cin >> n;
+
+    sieve();
+
+    vi a(n);
+    F0R(i, n) cin >> a[i];
+
+    vector<string> res(n);
+    int setwise = a[0];
+    FOR(i, 1, n) setwise = gcd(setwise, a[i]);
+
+    bool pairwise = true;
+    for(int x : a) {
+        if(!pairwise) break;
+
+        while(x != 1) {
+            int pr = lp[x];
+            while(x % pr == 0)
+                x /= pr;
+
+            if(seen[pr])
+                pairwise = false;
+            seen[pr] = true;
+        }
+    }
+    if(pairwise)
+        cout << "pairwise coprime\n";
+    else if(setwise == 1)
+        cout << "setwise coprime\n";
+    else
+        cout << "not coprime\n";
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int tc = 1;
+    FOR(i, 1, tc + 1) {
+        //cout << "Case #" << i << ": ";
+        solve();
+    }
+}
+
