bdaf03e5ea74ba63874f8e9f5bc9b2e5739debc9
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-09-14 00:26:53 +0200

    Codeforces: #670 (A, B)

---
+++ codeforces/1406: #670 (Div. 2)/B.cc
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define SZ(x) (int)(x).size()
+#define endl '\n'
+#define FOR(a, b, c) for (auto a = (b); (a) < (c); ++(a))
+#define F0R(a, b) FOR (a, 0, (b))
+#define CEIL(a, b) ((a) + (b)-1) / (b)
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return a > b ? a = b, true : false;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, true : false;
+}
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dout \
+  if (DEBUG) std::cerr
+#define dvar(...) " [" << #__VA_ARGS__ ": " << mt(__VA_ARGS__) << "] "
+template <typename T>
+struct IsC {
+  template <typename C>
+  static char test(typename C::const_iterator*);
+  template <typename C>
+  static int test(...);
+  static const bool value = sizeof(test<T>(0)) == sizeof(char);
+};
+template <>
+struct IsC<string> {
+  static const bool value = false;
+};
+template <typename C>
+typename enable_if<IsC<C>::value, ostream&>::type operator<<(ostream&, const C&);
+template <typename T1, typename T2>
+ostream& operator<<(ostream&, const pair<T1, T2>&);
+template <typename... T> using TS = tuple_size<tuple<T...>>;
+template<size_t idx, typename... T>
+typename enable_if<TS<T...>::value == idx + 1, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return o << ", " << get<idx>(t) << ')';
+}
+template<size_t idx, typename... T>
+typename enable_if<0 < idx && idx + 1 < TS<T...>::value, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return operator<<<idx + 1>(o << ", " << get<idx>(t), t);
+}
+template<size_t idx = 0, typename... T>
+typename enable_if<1 < TS<T...>::value && !idx, ostream&>::type
+operator<<(ostream& o, const tuple<T...>& t) {
+  return operator<<<idx + 1>(o << '(' << get<idx>(t), t);
+}
+template<typename T>
+ostream& operator<<(ostream& o, const tuple<T>& t) {
+  return o << get<0>(t);
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& o, const pair<T1, T2>& p) {
+  return o << mt(p.fi, p.se);
+}
+template <typename C>
+typename enable_if<IsC<C>::value, ostream&>::type operator<<(
+    ostream& o, const C& c) {
+  o << '[';
+  for (auto it = c.cbegin(); it != c.cend(); ++it)
+    o << *it << (next(it) != c.cend() ? ", " : "");
+  return o << ']';
+}
+template <typename T>
+void tprint(vector<vector<T>>& v, size_t width = 0, ostream& o = cerr) {
+  if (!DEBUG) return;
+  for (const auto& vv : v) {
+    for (const auto& i : vv) o << setw(width) << i;
+    o << endl;
+  }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<ll> ps, ns, a(n);
+    F0R(i, n) cin >> a[i];
+
+    copy_if(ALL(a), back_inserter(ps), [](int i) { return i > 0; });
+    copy_if(ALL(a), back_inserter(ns), [](int i) { return i < 0; });
+
+    sort(ALL(ns));
+    sort(RALL(ps));
+    sort(ALL(a), [&](int x, int y) { return abs(x) < abs(y); });
+
+    auto fp = [&](int i) -> ll { return i + 1 < SZ(ps) ? ps[i]*ps[i+1] : 0; };
+    auto fn = [&](int i) -> ll { return i + 1 < SZ(ns) ? ns[i]*ns[i+1] : 0; };
+    ll resNeg = accumulate(a.begin(), a.begin() + 5, 1ll, multiplies<ll>());
+    ll x = max( { fp(1)*fn(0), fp(1)*fp(3), fn(0)*fn(2) } );
+
+    if(!SZ(ps) || !x) {
+        cout << resNeg << endl;
+        return;
+    }
+
+    ll res = ps[0] * x;
+    cout << max(resNeg, res) << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int tc = 1;
+    cin >> tc;
+    FOR(i, 1, tc + 1) {
+        //cout << "Case #" << i << ": ";
+        solve();
+    }
+}
+
