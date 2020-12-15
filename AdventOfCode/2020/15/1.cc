04e89f7a8ed9f1c8d45a151494b423172f0dfea8
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-12-15 06:47:45 +0100

    AoC 2020: Day 15

---
+++ AdventOfCode/2020/15/1.cc
@@ -0,0 +1,21 @@
+#include "../template.cc"
+
+const int MAXN = 2020;
+
+void solve() {
+    vector<string> ss = getInp();
+    vector<string> numStr = split(ss[0], ",");
+    vi a;
+    for(string& s : numStr)
+        a.pb(stoi(s));
+
+    map<ll, ll> lastSpoken;
+    ll lastInterval = 0;
+    F0R(t, MAXN - 1) {
+        ll nextNum = t < SZ(a) ? a[t] : lastInterval;
+        lastInterval = lastSpoken.count(nextNum)*(t - lastSpoken[nextNum]);
+        lastSpoken[nextNum] = t;
+    }
+    cout << lastInterval << endl;
+}
+
