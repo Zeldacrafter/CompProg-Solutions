50cb98cace0f38826d2a1b29da01c71c4ddcef40
Alexander Pfanne <alex.pfanne1@gmail.com>
2021-12-04 06:34:35 +0100

    AdventOfCode 2021: Day 4

---
+++ AdventOfCode/2021/4/1.cc
@@ -0,0 +1,51 @@
+#include "../utils.cc"
+typedef vector<vvi> vvvi;
+
+bool wins(vvi& b) {
+    F0R(i, 5) {
+        bool r = true, c = true;
+        F0R(j, 5) r &= b[i][j], c &= b[j][i];
+        if(r || c) return true;
+    }
+    return false;
+}
+
+void solve() {
+    vector<ll> nums;
+    vvvi boards, boardsBool;
+    getInp([&](auto& cin, int i) {
+        if(!i) {
+            // First row
+            ll k; char c; cin >> k;
+            do {
+                nums.pb(k);
+            } while(cin >> c >> k);
+        } else {
+            i--;
+            if(i % 6 == 0) {
+                // New board
+                boards.pb(vvi(5, vi(5)));
+                boardsBool.pb(vvi(5, vi(5)));
+            } else {
+                // Next row
+                F0R(j, 5) cin >> boards.back()[i % 6 - 1][j];
+            }
+        }
+    });
+
+    for(ll n : nums) F0R(i, SZ(boards)) {
+        // Mark number
+        F0R(r, 5) F0R(c, 5)
+            boardsBool[i][r][c] |= boards[i][r][c] == n;
+        // Check won
+        if(wins(boardsBool[i])) {
+            ll unmarked = 0;
+            F0R(r, 5) F0R(c, 5)
+                unmarked += !boardsBool[i][r][c]*boards[i][r][c];
+            
+            cout << unmarked * n << endl;
+            return;
+        }
+    }
+}
+
