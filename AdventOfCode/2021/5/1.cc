9bb88da4248071f35022d9c2d7f01706f8f5ec54
Alexander Pfanne <alex.pfanne1@gmail.com>
2021-12-05 12:58:51 +0100

    AdventOfCode 2021: Day 5 - better

--- AdventOfCode/2021/5/1.cc
+++ AdventOfCode/2021/5/1.cc
@@ -8,7 +8,7 @@ void solve() {
 
         if(x1 == x2) {
             if(y1 > y2) swap(y1, y2);
-            FOR(i, y1, y2 + 1) amt[mp(x1,i)]++;
+            FOR(i, y1, y2 + 1) amt[mp(x1, i)]++;
         } else if (y1 == y2) {
             if(x1 > x2) swap(x1, x2);
             FOR(i, x1, x2 + 1) amt[mp(i, y1)]++;
@@ -17,8 +17,7 @@ void solve() {
 
     ll res = 0;
     for(auto [k, v] : amt)
-        if(v > 1)
-            res++;
+        res += v > 1;
     cout << res << endl;
 }
 
