98a8f0c79f254aac5a4c578e5603a9b6cfd0d955
Alexander Pfanne <alex.pfanne1@gmail.com>
2021-12-22 12:30:36 +0100

    Advent of Code: Day 22 - A bit better

--- AdventOfCode/2021/22/1.cc
+++ AdventOfCode/2021/22/1.cc
@@ -1,29 +1,22 @@
 #include "../utils.cc"
 
-typedef tuple<int, int, int, int, int, int> cube;
+typedef array<ii, 3> cube;
 
 bool contains(cube& a, cube& b) {
-    auto [xMnA, xMxA, yMnA, yMxA, zMnA, zMxA] = a;
-    auto [xMnB, xMxB, yMnB, yMxB, zMnB, zMxB] = b;
-
-    return xMnA <= xMnB && xMxA >= xMxB &&
-           yMnA <= yMnB && yMxA >= yMxB &&
-           zMnA <= zMnB && zMxA >= zMxB;
+    F0R(i, 3) 
+        if(a[i].fi > b[i].fi || a[i].se < b[i].se)
+            return false;
+    return true;
 }
 
 bool intersect(cube& a, cube& b) {
-    auto [xMnA, xMxA, yMnA, yMxA, zMnA, zMxA] = a;
-    auto [xMnB, xMxB, yMnB, yMxB, zMnB, zMxB] = b;
-
-    return xMnA <= xMxB && xMxA >= xMnB &&
-           yMnA <= yMxB && yMxA >= yMnB &&
-           zMnA <= zMxB && zMxA >= zMnB;
+    F0R(i, 3)
+        if(a[i].fi > b[i].se || a[i].se < b[i].fi)
+            return false;
+    return true;
 }
 
 void removeAndAdd(vector<cube>& newCs, cube a, cube b) {
-    auto [xMnA, xMxA, yMnA, yMxA, zMnA, zMxA] = a;
-    auto [xMnB, xMxB, yMnB, yMxB, zMnB, zMxB] = b;
-
     if(contains(b, a)) return;
 
     if(!intersect(a, b)) {
@@ -31,38 +24,33 @@ void removeAndAdd(vector<cube>& newCs, cube a, cube b) {
         return;
     }
 
-    auto getPts = [](int mnA, int mxA, int mnB, int mxB) {
-        vi splits;
-        splits.pb(mnA);
-        if(mnA < mnB && mnB < mxA) splits.pb(mnB);
-        if(mnA < mxB && mxB < mxA) splits.pb(mxB);
-        splits.pb(mxA);
-        return splits;
-    };
-    vi xPts = getPts(xMnA, xMxA, xMnB, xMxB),
-       yPts = getPts(yMnA, yMxA, yMnB, yMxB),
-       zPts = getPts(zMnA, zMxA, zMnB, zMxB);
+    vvi pts(3);
+    F0R(i, 3) {
+        pts[i].pb(a[i].fi);
+        if(a[i].fi < b[i].fi && b[i].fi < a[i].se) pts[i].pb(b[i].fi);
+        if(a[i].fi < b[i].se && b[i].se < a[i].se) pts[i].pb(b[i].se);
+        pts[i].pb(a[i].se);
+    }
 
-    F0R(i, SZ(xPts) - 1) F0R(j, SZ(yPts) - 1) F0R(k, SZ(zPts) - 1) {
-        cube c = mt(xPts[i], xPts[i + 1],
-                    yPts[j], yPts[j + 1],
-                    zPts[k], zPts[k + 1]);
+    F0R(i, SZ(pts[0]) - 1) F0R(j, SZ(pts[1]) - 1) F0R(k, SZ(pts[2]) - 1) {
+        cube c = { mp(pts[0][i], pts[0][i + 1]), 
+                   mp(pts[1][j], pts[1][j + 1]),
+                   mp(pts[2][k], pts[2][k + 1])};
         if(!contains(b, c)) newCs.pb(c);
     }
 }
 
-ll area(cube& a) {
-    auto [xMn, xMx, yMn, yMx, zMn, zMx] = a;
-    return ((ll)xMx - xMn) * (yMx - yMn) * (zMx - zMn);
+ll volume(cube& a) {
+    ll res = 1;
+    F0R(i, 3) res *= a[i].se - a[i].fi;
+    return res;
 }
 
 cube clamp(cube c, int v) {
-    ckmax(get<0>(c), -v);
-    ckmax(get<2>(c), -v);
-    ckmax(get<4>(c), -v);
-    ckmin(get<1>(c), v + 1);
-    ckmin(get<3>(c), v + 1);
-    ckmin(get<5>(c), v + 1);
+    F0R(i, 3) {
+        ckmax(c[i].fi, -v);
+        ckmin(c[i].se, v+5);
+    }
     return c;
 }
 
@@ -70,18 +58,17 @@ void solve() {
     vector<cube> cubes;
     getInp([&](auto& cin, int) {
         string s; char c;
-        int xMn, xMx, yMn, yMx, zMn, zMx;
-        cin >> s >> c >> c
-            >> xMn >> c >> c >> xMx
-            >> c >> c >> c
-            >> yMn >> c >> c >> yMx
-            >> c >> c >> c
-            >> zMn >> c >> c >> zMx;
+        cube newC;
+        F0R(i, 3) {
+            if(!i) cin >> s;
+            else cin >> c;
+            cin >> c >> c >> newC[i].fi >> c >> c >> newC[i].se;
+            ++newC[i].se;
+        }
+        F0R(i, 3)
+            if(newC[i].fi < -50 || newC[i].se > 51) return;
+        newC = clamp(newC, 50);
 
-        if(xMx < -50 || yMx < -50 || zMx < -50 ||
-           xMn >  50 || yMn >  50 || zMn >  50) return;
-
-        cube newC = clamp(mt(xMn, xMx+1, yMn, yMx+1, zMn, zMx+1), 50);
         vector<cube> newCubes;
         F0R(i, SZ(cubes))
             removeAndAdd(newCubes, cubes[i], newC);
@@ -92,7 +79,7 @@ void solve() {
 
     ll res = 0;
     for(cube& c : cubes)
-        res += area(c);
+        res += volume(c);
     cout << res << endl;
 }
 
--- AdventOfCode/2021/22/2.cc
+++ AdventOfCode/2021/22/2.cc
@@ -1,29 +1,22 @@
 #include "../utils.cc"
 
-typedef tuple<int, int, int, int, int, int> cube;
+typedef array<ii, 3> cube;
 
 bool contains(cube& a, cube& b) {
-    auto [xMnA, xMxA, yMnA, yMxA, zMnA, zMxA] = a;
-    auto [xMnB, xMxB, yMnB, yMxB, zMnB, zMxB] = b;
-
-    return xMnA <= xMnB && xMxA >= xMxB &&
-           yMnA <= yMnB && yMxA >= yMxB &&
-           zMnA <= zMnB && zMxA >= zMxB;
+    F0R(i, 3) 
+        if(a[i].fi > b[i].fi || a[i].se < b[i].se)
+            return false;
+    return true;
 }
 
 bool intersect(cube& a, cube& b) {
-    auto [xMnA, xMxA, yMnA, yMxA, zMnA, zMxA] = a;
-    auto [xMnB, xMxB, yMnB, yMxB, zMnB, zMxB] = b;
-
-    return xMnA <= xMxB && xMxA >= xMnB &&
-           yMnA <= yMxB && yMxA >= yMnB &&
-           zMnA <= zMxB && zMxA >= zMnB;
+    F0R(i, 3)
+        if(a[i].fi > b[i].se || a[i].se < b[i].fi)
+            return false;
+    return true;
 }
 
 void removeAndAdd(vector<cube>& newCs, cube a, cube b) {
-    auto [xMnA, xMxA, yMnA, yMxA, zMnA, zMxA] = a;
-    auto [xMnB, xMxB, yMnB, yMxB, zMnB, zMxB] = b;
-
     if(contains(b, a)) return;
 
     if(!intersect(a, b)) {
@@ -31,44 +24,39 @@ void removeAndAdd(vector<cube>& newCs, cube a, cube b) {
         return;
     }
 
-    auto getPts = [](int mnA, int mxA, int mnB, int mxB) {
-        vi splits;
-        splits.pb(mnA);
-        if(mnA < mnB && mnB < mxA) splits.pb(mnB);
-        if(mnA < mxB && mxB < mxA) splits.pb(mxB);
-        splits.pb(mxA);
-        return splits;
-    };
-    vi xPts = getPts(xMnA, xMxA, xMnB, xMxB),
-       yPts = getPts(yMnA, yMxA, yMnB, yMxB),
-       zPts = getPts(zMnA, zMxA, zMnB, zMxB);
+    vvi pts(3);
+    F0R(i, 3) {
+        pts[i].pb(a[i].fi);
+        if(a[i].fi < b[i].fi && b[i].fi < a[i].se) pts[i].pb(b[i].fi);
+        if(a[i].fi < b[i].se && b[i].se < a[i].se) pts[i].pb(b[i].se);
+        pts[i].pb(a[i].se);
+    }
 
-    F0R(i, SZ(xPts) - 1) F0R(j, SZ(yPts) - 1) F0R(k, SZ(zPts) - 1) {
-        cube c = mt(xPts[i], xPts[i + 1],
-                    yPts[j], yPts[j + 1],
-                    zPts[k], zPts[k + 1]);
+    F0R(i, SZ(pts[0]) - 1) F0R(j, SZ(pts[1]) - 1) F0R(k, SZ(pts[2]) - 1) {
+        cube c = { mp(pts[0][i], pts[0][i + 1]), 
+                   mp(pts[1][j], pts[1][j + 1]),
+                   mp(pts[2][k], pts[2][k + 1])};
         if(!contains(b, c)) newCs.pb(c);
     }
 }
 
-ll area(cube& a) {
-    auto [xMn, xMx, yMn, yMx, zMn, zMx] = a;
-    return ((ll)xMx - xMn) * (yMx - yMn) * (zMx - zMn);
+ll volume(cube& a) {
+    ll res = 1;
+    F0R(i, 3) res *= a[i].se - a[i].fi;
+    return res;
 }
 
 void solve() {
     vector<cube> cubes;
     getInp([&](auto& cin, int) {
         string s; char c;
-        int xMn, xMx, yMn, yMx, zMn, zMx;
-        cin >> s >> c >> c
-            >> xMn >> c >> c >> xMx
-            >> c >> c >> c
-            >> yMn >> c >> c >> yMx
-            >> c >> c >> c
-            >> zMn >> c >> c >> zMx;
-
-        cube newC = mt(xMn, xMx+1, yMn, yMx+1, zMn, zMx+1);
+        cube newC;
+        F0R(i, 3) {
+            if(!i) cin >> s;
+            else cin >> c;
+            cin >> c >> c >> newC[i].fi >> c >> c >> newC[i].se;
+            ++newC[i].se;
+        }
         vector<cube> newCubes;
         F0R(i, SZ(cubes))
             removeAndAdd(newCubes, cubes[i], newC);
@@ -79,7 +67,7 @@ void solve() {
 
     ll res = 0;
     for(cube& c : cubes)
-        res += area(c);
+        res += volume(c);
     cout << res << endl;
 }
 
