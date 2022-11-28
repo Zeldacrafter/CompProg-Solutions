11e5c20c0de5a335cc7b1f6beddde3459c4c06e1
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-11-28 21:53:07 +0100

    Setup AdventOfCode 2022

---
+++ AdventOfCode/2022/utils.cc
@@ -0,0 +1,81 @@
+#pragma once
+#include "/home/al/git/CompProg/code/utils/ops.cc"
+
+///////////////////////////////////////////////////////////////
+// https://github.com/Zeldacrafter/CompProg/blob/master/code/utils/y_combinator.cc
+
+// for c++14 and above;  explicitly specify the return type of the lambda
+// links:
+//  1. https://stackoverflow.com/a/40873657
+//  2. http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<typename F>
+struct y_combinator_result {
+  F f;
+  template<typename T>
+  explicit y_combinator_result(T&& fun) : f(forward<T>(fun)) {}
+  template<typename... Args>
+  decltype(auto) operator()(Args&&... args) {
+    return f(ref(*this), forward<Args>(args)...);
+  }
+};
+template<typename F>
+decltype(auto) y_combinator(F&& f) {
+  return y_combinator_result<decay_t<F>>(forward<F>(f));
+}
+
+///////////////////////////////////////////////////////////////
+// Reading input line by line
+///////////////////////////////////////////////////////////////
+
+template<typename F>
+vector<string> getInp(F f) {
+    vector<string> res;
+    string s;
+    int emptyLineCounter = 0;
+    for (int i = 0; getline(cin, s); ++i, res.pb(s)) {
+        if (s == "") emptyLineCounter++;
+        stringstream inp(s);
+        f(inp, i);
+    }
+    if (emptyLineCounter)
+        dout << "Number of empty lines: " << emptyLineCounter << endl;
+    return res;
+}
+
+vector<string> getInp() {
+    return getInp([](stringstream&, int) {});
+}
+
+vector<string> split(const string& s, const string& delim) {
+    vector<string> res;
+    size_t last = 0, next = 0;
+    while((next = s.find(delim, last)) != string::npos) {
+        res.pb(s.substr(last, next - last));
+        last = next + SZ(delim);
+    }
+    res.pb(s.substr(last));
+    return res;
+}
+
+pair<string, string> splitP(const string& s, const string& delim) {
+    vector<string> ss = split(s, delim);
+    static bool didError = false;
+    if(SZ(ss) != 2 && !didError) {
+        dout << "Warning: \"" << s << "\" split with delim \"" << delim << "\" prodces " << ss << endl;
+        didError = true;
+    }
+    return mp(ss[0], ss[1]);
+}
+
+///////////////////////////////////////////////////////////////
+// Begin Solution
+///////////////////////////////////////////////////////////////
+
+void solve();
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    solve();
+}
