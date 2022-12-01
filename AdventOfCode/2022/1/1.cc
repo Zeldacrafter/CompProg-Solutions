f1dd26ccbf1fc6af3b99cdbebcbf73ec3e0fe615
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-12-01 08:02:28 +0100

    AdventOfCode 2022: Day 1

---
+++ AdventOfCode/2022/1/1.cc
@@ -0,0 +1,21 @@
+#include "../utils.cc"
+
+void solve() {
+    vector<string> ss = getInp();
+    ss.pb("");
+
+    ll curr = 0;
+    vector<ll> totals;
+    for(string& s : ss) {
+        if(s == "") {
+            totals.pb(curr);
+            curr = 0;
+        } else {
+            curr += stoll(s);
+        }
+    }
+    sort(RALL(totals));
+
+    cout << totals[0] << endl;
+}
+
