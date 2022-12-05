ed1effbcf3ffeddfe2ac40d79f1e7ad35fcb71a2
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-12-05 06:24:26 +0100

    AdventOfCode 2022: Day 5

---
+++ AdventOfCode/2022/5/1.cc
@@ -0,0 +1,30 @@
+#include "../utils.cc"
+
+vector<string> stacks = {
+    "DBJV",
+    "PVBWRDF",
+    "RGFLDCWQ",
+    "WJPMLNDB",
+    "HNBPCSQ",
+    "RDBSNG",
+    "ZBPMQFSH",
+    "WLF",
+    "SVFMR"
+};
+
+void solve() {
+
+    getInp([&](auto& cin, int i) {
+        if (i <= 9) return;
+
+        string s; int amt, from, to;
+        cin >> s >> amt >> s >> from >> s >> to;
+        from--; to--;
+
+        stacks[to].insert(stacks[to].end(), RALL(stacks[from]) - (SZ(stacks[from]) - amt));
+        stacks[from].resize(SZ(stacks[from]) - amt);
+    });
+
+    for(string& s : stacks) cout << s.back();
+}
+
