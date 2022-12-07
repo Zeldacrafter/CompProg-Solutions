2cc344689577a7234643e966fe137e42868380d3
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-12-07 06:42:25 +0100

    AdventOfCode 2022: Day 7

---
+++ AdventOfCode/2022/7/1.cc
@@ -0,0 +1,51 @@
+#include "../utils.cc"
+
+void solve() {
+    vector<string> curr;
+    string currDir;
+    map<string, ll> size;
+    map<string, bool> done;
+    bool first = true;
+
+    getInp([&](auto& cin, int) {
+        if(cin.peek() == '$') {
+            char c; string comm; cin >> c >> comm;
+
+            if(comm == "cd") {
+                string s; cin >> s;
+
+                if(!first) done[currDir] = true;
+
+                if(s == "/") {
+                    curr.clear();
+                    currDir = "";
+                } else if(s == "..") {
+                    currDir = currDir.substr(0, SZ(currDir) - SZ(curr.back()) - 1);
+                    curr.pop_back();
+                } else {
+                    curr.pb(s);
+                    currDir += "/" + s;
+                }
+                first = false;
+            } 
+        } else {
+            if(cin.peek() != 'd' && !done[currDir]) {
+                ll sz; cin >> sz;
+
+                string dir = "";
+                size[dir] += sz;
+                for(string& d : curr) {
+                    dir += "/" + d;
+                    size[dir] += sz;
+                }
+            }
+        }
+    });
+
+    ll res = 0;
+    for(auto [k, v] : size)
+        if(v <= 100000) 
+            res += v;
+    cout << res << endl;
+}
+
