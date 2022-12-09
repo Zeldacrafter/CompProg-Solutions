31e0a4ecb30aa5035dbf352673d8d760078a592d
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-12-09 06:54:23 +0100

    AdventOfCode 2022: Day 9

---
+++ AdventOfCode/2022/9/1.cc
@@ -0,0 +1,45 @@
+#include "../utils.cc"
+
+#define N 2
+
+int sgn(int val) {
+    return (0 < val) - (val < 0);
+}
+
+void solve() {
+    set<ii> seen;
+
+    vi xs(N), ys(N);
+
+    getInp([&](auto& cin, int) {
+        char c; int dist; cin >> c >> dist;
+
+        F0R(i, dist) {
+            switch(c) {
+                case 'R': xs[0]++; break;
+                case 'L': xs[0]--; break;
+                case 'U': ys[0]++; break;
+                case 'D': ys[0]--; break;
+            }
+
+            FOR(i, 1, N) {
+                int dx = xs[i - 1] - xs[i],
+                    dy = ys[i - 1] - ys[i];
+
+                if(!dx && abs(dy) == 2)
+                    ys[i] += sgn(dy);
+                else if (!dy && abs(dx) == 2)
+                    xs[i] += sgn(dx);
+                else if(abs(dx) + abs(dy) > 2) {
+                    ys[i] += sgn(dy);
+                    xs[i] += sgn(dx);
+                }
+            }
+
+            seen.insert(mp(xs.back(), ys.back()));
+        }
+    });
+    cout << SZ(seen) << endl;
+
+}
+
