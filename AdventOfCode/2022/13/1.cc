6f5705865101876fff1af867f8df1b98baebc7f0
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-12-14 15:35:20 +0100

    AdventOfCode 2022: Day 13 cpp

---
+++ AdventOfCode/2022/13/1.cc
@@ -0,0 +1,28 @@
+# today in python
+import fileinput
+import numpy as np
+
+
+def comp(v1, v2):
+    if isinstance(v1, list) and isinstance(v2, list):
+        for i in range(max(len(v1), len(v2))):
+            if i >= len(v1): return -1
+            if i >= len(v2): return 1
+            cmp = comp(v1[i], v2[i])
+            if cmp != 0: return cmp
+        return 0
+    elif isinstance(v1, list):
+        return comp(v1, [v2])
+    elif isinstance(v2, list):
+        return comp([v1], v2)
+    else:
+        return np.sign(v1 - v2)
+
+def lt(v1, v2): return comp(v1, v2) == -1
+
+res1 = 0
+inp = list(fileinput.input())
+for i in range((len(inp)+2)//3):
+    if lt(eval(inp[3*i]), eval(inp[3*i+1])):
+        res1 += 1 + i
+print(res1)
---
+++ AdventOfCode/2022/13/2.cc
@@ -0,0 +1,28 @@
+# today in python
+import fileinput
+import numpy as np
+from functools import cmp_to_key
+
+def comp(v1, v2):
+    if isinstance(v1, list) and isinstance(v2, list):
+        for i in range(max(len(v1), len(v2))):
+            if i >= len(v1): return -1
+            if i >= len(v2): return 1
+            cmp = comp(v1[i], v2[i])
+            if cmp != 0: return cmp
+        return 0
+    elif isinstance(v1, list):
+        return comp(v1, [v2])
+    elif isinstance(v2, list):
+        return comp([v1], v2)
+    else:
+        return np.sign(v1 - v2)
+
+allPkgs = [ [[2]], [[6]] ]
+for l in fileinput.input():
+    if l != "\n":
+        allPkgs += [eval(l)]
+
+allPkgs.sort(key=cmp_to_key(comp))
+
+print((allPkgs.index([[2]]) + 1)*(allPkgs.index([[6]]) + 1))
