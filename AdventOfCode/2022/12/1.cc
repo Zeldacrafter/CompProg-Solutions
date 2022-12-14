592c8961d6075adc11a1455d1c3b670398a8df63
Alexander Pfanne <alex.pfanne1@gmail.com>
2022-12-14 14:10:57 +0100

    AdventOfCode 2022: Day 12

---
+++ AdventOfCode/2022/12/1.cc
@@ -0,0 +1,51 @@
+#include "../utils.cc"
+
+const vi DR = {1, -1, 0, 0},
+         DC = {0, 0, 1, -1};
+
+void solve() {
+    vector<string> ss = getInp();
+
+    ii start, end;
+    F0R(r, SZ(ss)) F0R(c, SZ(ss[0])) {
+        if(ss[r][c] == 'S') {
+            start = mp(r, c);
+            ss[r][c] = 'a';
+        }
+        if(ss[r][c] == 'E') {
+            end = mp(r, c);
+            ss[r][c] = 'z';
+        }
+    }
+            
+    auto find = [&](ii s) {
+        queue<tuple<int, int, int>> q;
+        q.emplace(s.fi, s.se, 0);
+        set<ii> seen;
+        seen.insert(s);
+
+        while(SZ(q)) {
+            auto [r, c, d] = q.front();
+            q.pop();
+
+            if(mp(r, c) == end) return d;
+
+            F0R(i, 4) {
+                int rr = r + DR[i],
+                    cc = c + DC[i];
+
+                if(min(rr,cc) < 0 || rr >= SZ(ss) || cc >= SZ(ss[0]) ||
+                        seen.count(mp(rr, cc)) || 
+                        ss[rr][cc] - ss[r][c] > 1)
+                    continue;
+
+                q.emplace(rr, cc, d + 1);
+                seen.emplace(rr, cc);
+            }
+        }
+        return (int)1e9;
+    };
+
+    cout << find(start) << endl;
+}
+
