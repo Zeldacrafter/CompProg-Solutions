9f62a13720025f3f72e57eba601af5579d28f52d
Alexander Pfanne <alex.pfanne1@gmail.com>
2023-11-29 00:16:31 +0100

    .

--- AdventOfCode/2022/16/2.cc
+++ AdventOfCode/2022/16/2.cc
@@ -42,17 +42,20 @@ void solve() {
     dp[0][start][start][0] = 0;
     F0R (i, 26) F0R (j, n) F0R (k, n) F0R (l, 1 << npos) {
         if (!~dp[i][j][k][l]) continue;
+
         vector<tuple<int, int, int>> a, b;
+
         if (~pos[j] && !((l >> pos[j]) & 1))
             a.eb(j, l | (1 << pos[j]), (26 - i - 1) * flow[j]);
+        for (int u : adj[j]) a.eb(u, l, 0);
+
         if (k != j && ~pos[k] && !((l >> pos[k]) & 1))
             b.eb(k, l | (1 << pos[k]), (26 - i - 1) * flow[k]);
-        for (int u : adj[j]) a.eb(u, l, 0);
         for (int u : adj[k]) b.eb(u, l, 0);
         
-        for (auto [vv, ov, vval] : a)
-            for (auto [pp, op, pval] : b)
-                ckmax(dp[i + 1][vv][pp][ov | op], dp[i][j][k][l] + vval + pval);
+        for (auto [c1, o1, v1] : a)
+            for (auto [c2, o2, v2] : b)
+                ckmax(dp[i + 1][c1][c2][o1 | o2], dp[i][j][k][l] + v1 + v2);
     }
     int best = 0;
     F0R (j, n) F0R (k, n) F0R (l, 1 << npos) ckmax(best, dp[26][j][k][l]);
