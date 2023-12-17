043a51c9498c40774c863e09e1399f25621e8071
Alexander Pfanne <alex.pfanne1@gmail.com>
2023-12-17 07:23:31 +0100

    AdventOfCode 2023: Day 17

---
+++ AdventOfCode/2023/17/2.cc
@@ -0,0 +1,45 @@
+#include "../utils.cc"
+
+const vii dirs { mp(1, 0), mp(-1, 0), mp(0, 1), mp(0, -1) };
+
+void solve() {
+    vector<string> ss = getInp();
+
+    // dist, pos, previous dir
+    set<tuple<ll, ii, ii>> q;
+    map<pair<ii, ii>, ll> dist;
+    for(ii dir : { mp(1, 0), mp(0, 1) }) {
+        q.emplace(0, mp(0, 0), dir);
+        dist[{mp(0, 0), dir}] = 0;
+    }
+
+    while(SZ(q)) {
+        auto [d, p, dir] = *q.begin();
+        auto [r, c] = p;
+        q.erase(q.begin());
+
+        if(dist[{p, dir}] < d) continue;
+        if(r == SZ(ss) - 1 && c == SZ(ss[r]) - 1) {
+            cout << d << endl;
+            return;
+        }
+
+        F0R (i, 4) {
+            if ((dirs[i] & dir) != mp(0, 0)) continue;
+            ll dNew = d;
+            FOR (j, 1, 11) {
+                ii pp = p + j*dirs[i];
+                auto [rr, cc] = pp;
+
+                if (min(cc, rr) < 0 || rr >= SZ(ss) || cc >= SZ(ss[rr])) break;
+
+                dNew += ss[rr][cc] - '0';
+                if (j >= 4 && (!dist.count({pp, dirs[i]}) || dist[{pp, dirs[i]}] > dNew)) {
+                    dist[{pp, dirs[i]}] = dNew;
+                    q.emplace(dNew, pp, dirs[i]);
+                }
+            }
+        }
+    }
+    assert(false);
+}
