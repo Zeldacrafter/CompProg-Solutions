940a393bc70bd4bbf84c88ad620ce13983c23ade
Alexander Pfanne <alex.pfanne1@gmail.com>
2024-12-07 06:15:21 +0100

    AoC 2024: Day 7

---
+++ AdventOfCode/2024/7/1.cc
@@ -0,0 +1,20 @@
+#include "../utils.cc"
+
+bool is_ok(ll val, vi& a, ll curr = 0, int i = 0) {
+    if(i == SZ(a)) return val == curr;
+
+    return is_ok(val, a, curr + a[i], i + 1) 
+        || is_ok(val, a, curr * a[i], i + 1);
+}
+
+void solve() {
+    ll res = 0;
+    getInp([&](auto& cin, int) {
+        ll val, y; vi a; char _;
+        cin >> val >> _;
+        while(cin >> y) a.pb(y);
+        if(is_ok(val, a)) res += val;
+    });
+    cout << res << endl;
+}
+
